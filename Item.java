f9e9fdcc99820402cdc2eda5dce880f3b0fa0124
Jen369 <jennifer.martinez22@utrgv.edu>
2025-04-24 15:06:44 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,9 +1,17 @@
 public class Item{
-    private int amount;
-    private String name;
+    protected int amount;
+    protected String name;
 
-    public void set_amount(int val){};
-    public void get_amount(){};
-    public void set_name(String title){};
-    public void get_name(){};
+    public void set_amount(int val){
+        this.amout = val;
+    }
+    public void get_amount(){
+        return amount;
+    }
+    public void set_name(String title){
+        this.name = title;
+    }
+    public void get_name(){
+        return name;
+    }
 }
